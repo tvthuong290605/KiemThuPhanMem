b1b733e34945de530eec9038438ad009956d26e3
tvthuong290605 <tvthuong290605@gmail.com>
2025-12-09 08:42:54 +0700

    Delete frontend/cypress/support directory

--- frontend/cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-// cypress/support/commands.js
-
-Cypress.Commands.add('login', (username = 'testuser', password = 'Test123') => {
-  cy.visit('/auth');
-  cy.get('input[name="username"]').type(username);
-  cy.get('input[name="password"]').type(password);
-  cy.get('button[type="submit"]').click();
-});
--- frontend/cypress/support/e2e.js
+++
@@ -1,5 +0,0 @@
-// cypress/support/e2e.js
-
-import './commands';
-
-Cypress.on('uncaught:exception', () => false);
